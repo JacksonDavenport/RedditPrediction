547589cdfb93c110b5906c6b5b18d6922a92a903
Jack <j1davenp@ucsd.edu>
2016-04-21 14:53:51 -0700

    Created Python code to run Java CreateDistributions for each subreddit inputted

---
+++ Generate_Distributions.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+# Validate Input
+if(len(sys.argv) < 2):
+    print ("Invalid input, please include subreddit name(s)")
+    sys.exit()
+
+# Diagnostics
+print ("Generate Distributions for:")
+for subreddit_name in (sys.argv[1:]):
+    print ("\t/r/", subreddit_name)
+	
+# Scroll through each subreddit included
+for subreddit_name in (sys.argv[1:]):
+    command = "Java CreateDistributions " + subreddit_name
+    os.system(command)
+
+
+
+
