c0cd60b5e4cd1d52cbf3783b37ddeef7c129a3b5
JacksonDavenport <jakdavenport@yahoo.com>
2016-10-23 16:05:35 -0700

    Modified to -1/0/1 Comparator to accomodate low likelihoods

--- LikelihoodComparator.java
+++ LikelihoodComparator.java
@@ -3,6 +3,12 @@ import java.util.Comparator;
 public class LikelihoodComparator implements Comparator<Result> {
 	@Override
 	public int compare(Result r1, Result r2) {
-		return (int) (r2.getLikelihood() - r1.getLikelihood());
+		if(r2.getLikelihood() > r1.getLikelihood()){
+			return 1;
+		}
+		if(r2.getLikelihood() < r1.getLikelihood()){
+			return -1;
+		}
+		return 0;
 	}
 }
